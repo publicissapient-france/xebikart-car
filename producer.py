f4c4ad2f2a19930fe683be12788ef505dc240dc1
Paul-Guillaume Déjardin <pgdejardin@xebia.fr>
2019-03-12 10:17:15 +0100

    :tada: Client + Producer + Docker rabbitmq mqtt

---
+++ producer.py
@@ -0,0 +1,27 @@
+import paho.mqtt.client as mqtt
+
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+
+
+def on_publish(client, userdata, result):
+    print(f"data published ${result} \n")
+
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+client.on_publish = on_publish
+client.username_pw_set(username="admin", password="admin")
+client.connect("localhost", 1883, 60)
+
+rc = 0
+
+while rc == 0:
+    rc = client.loop()
+    client.publish("hello/world", "Hello, World!")
