d320212adcba9c57940390a1378454356f463655
Nicolas Laille <nlaille@xebia.fr>
2019-10-07 11:31:44 +0200

    fix intel-tensorflow patch => upgrade numpy

--- car-ml/setup.py
+++ car-ml/setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.1",
     packages=find_packages(),
     install_requires=[
-        'numpy==1.14.5',
+        'numpy>=1.14.5',
         'requests==2.22.0',
         'tensorflow==1.14.0',
         'donkeycar'
