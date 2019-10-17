08be68adfaf42551ed8f2ad065b7989362923836
Nicolas Laille <nlaille@xebia.fr>
2019-10-17 14:08:45 +0200

    add gym stack obs wrapper

--- car-ml/xebikart/gym/envs/__init__.py
+++ car-ml/xebikart/gym/envs/__init__.py
@@ -30,3 +30,34 @@ def create_env(vae, level=4, frame_skip=2, max_cte_error=3.0,
     return history_obs
 
 
+def create_stack_obs_clipping_steering(vae, level=4, frame_skip=2, max_cte_error=3.0,
+                                       min_steering=-1, max_steering=1,
+                                       min_throttle=0.1, max_throttle=0.3,
+                                       max_steering_diff=0.15, num_stack=4,
+                                       headless=True, reward_fn=None):
+
+    from xebikart.gym.envs.donkey_env import DonkeyEnv
+    from xebikart.gym.envs.wrappers import CropObservationWrapper, ConvVariationalAutoEncoderObservationWrapper, \
+        EdgingObservationWrapper, ClipSteeringBasedOnPrevious
+    from xebikart.gym.envs.gym_wrappers import FrameStack
+
+    # Create donkey env
+    donkey_env = DonkeyEnv(
+      level=level, frame_skip=frame_skip, max_cte_error=max_cte_error,
+      min_steering=min_steering, max_steering=max_steering,
+      min_throttle=min_throttle, max_throttle=max_throttle,
+      headless=headless, reward_fn=reward_fn
+    )
+
+    # CropObservation
+    crop_obs = CropObservationWrapper(donkey_env, 0, 40, 160, 80)
+    # Edging
+    edging_obs = EdgingObservationWrapper(crop_obs)
+    # VAE
+    vae_obs = ConvVariationalAutoEncoderObservationWrapper(edging_obs, vae)
+    # Stack
+    stack_obs = FrameStack(vae_obs, num_stack=num_stack)
+    # Clip steering action
+    clip_action = ClipSteeringBasedOnPrevious(stack_obs, max_steering_diff=max_steering_diff)
+
+    return clip_action
