7526a4608dce52c86fc0e38dd3b739256e78ef0a
nlaille <nlaille@xebia.fr>
2019-11-03 19:19:03 +0100

    change import order

--- car-ml/convert_to_tflite.py
+++ car-ml/convert_to_tflite.py
@@ -2,6 +2,7 @@ import argparse
 
 import mlflow.keras
 
+from xebikart.lite_functions import keras_session_to_tflite
 
 parser = argparse.ArgumentParser(description='Execute and log notebook to mlflow')
 parser.add_argument('--runid', dest='runid', required=True,
@@ -11,11 +12,7 @@ parser.add_argument('--model-path', dest='model_path', required=True,
 parser.add_argument('--output-path', dest='output_path', required=True,
                     help='path to save the model')
 
-
 args = parser.parse_args()
 
 model = mlflow.keras.load_model(f"runs:/{args.runid}/{args.model_path}")
-
-from xebikart.lite_functions import keras_session_to_tflite
-
 keras_session_to_tflite(model, args.output_path)
