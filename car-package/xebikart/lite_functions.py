58bcc68e749b244e7b2d1f0ce1bed4efda217fdb
nlaille <nlaille@xebia.fr>
2019-11-03 19:17:39 +0100

    add function to convert keras model to tflite

--- car-package/xebikart/lite_functions.py
+++ car-package/xebikart/lite_functions.py
@@ -23,17 +23,24 @@ def interpreter_and_details(model_path) :
     return interpreter, input_details, output_details
 
 
-def predictor_builder(model_path, preprocess_fn):
+def keras_session_to_tflite(model, out_filename):
+    inputs = model.inputs
+    outputs = model.outputs
+    with tf.keras.backend.get_session() as sess:
+        converter = tf.lite.TFLiteConverter.from_session(sess, inputs, outputs)
+        tflite_model = converter.convert()
+        open(out_filename, "wb").write(tflite_model)
+
+
+def predictor_builder(model_path):
     """
-    get a predictor from a lite model and the corresponding preprocess
+    get a predictor from a lite model
     :param model_path: lite model path
-    :param preprocess_fn: function used to process the image
     :return: predictor : a function that take a tf image and make the prediction
     """
     interpreter, input_details, output_details = interpreter_and_details(model_path)
 
     def predictor(input_image):
-        input_image = preprocess_fn(input_image)
         input_image = tf.expand_dims(input_image)
 
         interpreter.set_tensor(input_details[0]['index'], input_image)
@@ -42,6 +49,7 @@ def predictor_builder(model_path, preprocess_fn):
         # The function `get_tensor()` returns a copy of the tensor data.
         # Use `tensor()` in order to get a pointer to the tensor.
         output_data = interpreter.get_tensor(output_details[0]['index'])
+        output_data = tf.squeeze(output_data)
 
         return output_data
 
