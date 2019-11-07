3a7bff78ffeafd56882622c1061c1e9cb47cae83
nlaille <nlaille@xebia.fr>
2019-11-07 13:03:21 +0100

    define basic joystick

--- car-package/xebikart/parts/joystick.py
+++ car-package/xebikart/parts/joystick.py
@@ -2,43 +2,45 @@ from donkeypart_ps3_controller import PS3JoystickController
 from xebikart.parts.driver import KeynoteDriver
 
 
-class KeynoteJoystick(PS3JoystickController):
+class Joystick(PS3JoystickController):
 
     def __init__(self, *args, **kwargs):
-        super(KeynoteJoystick, self).__init__(*args, **kwargs)
-        self.default_actions = {
-            KeynoteDriver.EMERGENCY_STOP: False,
-            KeynoteDriver.MODE_TOGGLE: False
-        }
-        self.reset_actions()
+        super(Joystick, self).__init__(*args, **kwargs)
+        self.actions = {}
 
     def init_js(self):
-        super(KeynoteJoystick, self).init_js()
+        super(Joystick, self).init_js()
         if self.js is None:
             raise RuntimeError("Unable to start Joystick Controller")
 
     def init_trigger_maps(self):
-        super(KeynoteJoystick, self).init_trigger_maps()
+        super(Joystick, self).init_trigger_maps()
 
-        self.button_down_trigger_map = {
-            'cross': self.record_action(KeynoteDriver.EMERGENCY_STOP),
-            'square': self.record_action(KeynoteDriver.EXIT_SAFE_MODE),
-            'select': self.record_action(KeynoteDriver.MODE_TOGGLE)
-        }
+        self.button_down_trigger_map = {}
 
         self.button_up_trigger_map = {}
+
+    def add_trigger_action(self, button, action):
+        self.button_down_trigger_map[button] = action
+        self.default_actions[action] = False
 
     def record_action(self, action):
         def _fn():
             self.actions[action] = True
         return _fn
 
-    def reset_actions(self):
-        self.actions = self.default_actions.copy()
-
     def run_threaded(self):
-        actions = [k for k, v in self.actions.items() if v is True]
+        actions = self.actions.copy()
         # reset actions
-        self.reset_actions()
+        self.actions = {}
 
         return self.angle, self.throttle, actions
+
+
+class KeynoteJoystick(Joystick):
+
+    def __init__(self, *args, **kwargs):
+        super(KeynoteJoystick, self).__init__(*args, **kwargs)
+        self.add_trigger_action('cross', KeynoteDriver.EMERGENCY_STOP)
+        self.add_trigger_action('square', KeynoteDriver.EXIT_SAFE_MODE)
+        self.add_trigger_action('select', KeynoteDriver.MODE_TOGGLE)
