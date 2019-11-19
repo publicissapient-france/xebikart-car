37f900776601123ccceb269d635bbb6fb46d7877
Sergio Dos Santos <sergiords@users.noreply.github.com>
2019-11-19 11:08:14 +0100

    add discrete measures class for Lidar, refactor position computing functions: rename methods and variables

--- car-package/xebikart/parts/lidar.py
+++ car-package/xebikart/parts/lidar.py
@@ -5,7 +5,14 @@ import logging
 
 import config
 from box import MinimumBoundingBox
-from rplidar import RPLidar
+from rplidar import RPLidar as rpl
+
+ANGLES_SLOTS = 36
+ANGLE_MAX = 360
+ANGLES_NORTH_SLOT = 0  # index for northern angle
+ANGLES_SOUTH_SLOT = ANGLES_SLOTS // 2  # shift in index to retrieve southern angle
+ANGLES_EAST_SLOT = ANGLES_SLOTS // 4  # shift in index to retrieve eastern angle
+ANGLES_WEST_SLOT = ANGLES_EAST_SLOT + ANGLES_SOUTH_SLOT  # shift in index to retrieve western angle
 
 
 # These samples were extracted and adapted from donkeycar parts samples. Original version can be found here:
@@ -18,7 +25,7 @@ class RPLidar(object):
     '''
 
     def __init__(self, port='/dev/ttyUSB0'):
-        self.lidar = RPLidar(port)
+        self.lidar = rpl(port)
         self.lidar.clear_input()
         time.sleep(1)
         self.scan = None
@@ -71,14 +78,14 @@ def measures_to_positions(measures):
 def rotate(point, angle):
     rotation_angle = -angle
     x, y = point
-    rotated_x = math.cos(rotation_angle) * x - math.sin(rotation_angle) * y
-    rotated_y = math.sin(rotation_angle) * x + math.cos(rotation_angle) * y
+    rotated_x = (math.cos(rotation_angle) * x) - (math.sin(rotation_angle) * y)
+    rotated_y = (math.sin(rotation_angle) * x) + (math.cos(rotation_angle) * y)
     return rotated_x, rotated_y
 
 
-def points_to_position_tuple(points):
-    xs = [int(x) for (x, y) in points]
-    ys = [int(y) for (x, y) in points]
+def corner_points_to_positions(corner_points):
+    xs = [int(x) for (x, y) in corner_points]
+    ys = [int(y) for (x, y) in corner_points]
     x_min = abs(min(xs))
     y_min = abs(min(ys))
     x_max = max(xs)
@@ -86,19 +93,28 @@ def points_to_position_tuple(points):
     return (x_min, y_min), (x_max, y_max)
 
 
-def next_location(current_location, position_tuple, angle):
-    (position1, position2) = position_tuple
+def next_location(current_location, positions, raw_angle):
+    (position1, position2) = positions
     current_position = (current_location.x, current_location.y)
     next_position = position1 \
         if distance(position1, current_position) < distance(position2, current_position) \
         else position2
-    return Location(angle, next_position[0], next_position[1])
+    next_angle = int(math.degrees(raw_angle) % 360)
+    return Location(next_angle, next_position[0], next_position[1])
 
 
 def distance(p1, p2):
     (x1, y1) = p1
     (x2, y2) = p2
     return ((x1 - x2) ** 2) + ((y1 - y2) ** 2)
+
+
+def discrete_measures(measures):
+    angles = [0 for x in range(ANGLES_SLOTS)]
+    for measure in measures:
+        index = int(measure[1] * ANGLES_SLOTS // ANGLE_MAX)
+        angles[index] = max(angles[index], measure[2])
+    return angles
 
 
 class LidarPosition:
@@ -115,10 +131,10 @@ class LidarPosition:
                 continue
             positions = measures_to_positions(self.measures)
             bounding_box = MinimumBoundingBox(positions)
-            angle = bounding_box.unit_vector_angle
-            rotated_points = [rotate(point, angle) for point in bounding_box.corner_points]
-            position_tuple = points_to_position_tuple(rotated_points)
-            self.location = next_location(self.location, position_tuple, int(math.degrees(angle) % 360))
+            bounding_box_angle = bounding_box.unit_vector_angle
+            rotated_corner_points = [rotate(point, bounding_box_angle) for point in bounding_box.corner_points]
+            positions = corner_points_to_positions(rotated_corner_points)
+            self.location = next_location(self.location, positions, bounding_box_angle)
 
     def run_threaded(self, scan):
         return self.run(scan)
@@ -127,6 +143,18 @@ class LidarPosition:
         if scan is not None and len(scan) > 0:
             self.measures = list(map(lambda item: Measure(item[1], item[2]), scan))
         return self.location
+
+    def shutdown(self):
+        self.on = False
+
+
+class LidarDistances:
+
+    def run(self, scan):
+        result = []
+        if scan is not None and len(scan) > 0:
+            result = discrete_measures(scan)
+        return result
 
     def shutdown(self):
         self.on = False
